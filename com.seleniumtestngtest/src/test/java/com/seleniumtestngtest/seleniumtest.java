40994df48dff7ead631fcd6c258198b62a52b598
Fransiskusandika <dikakoko@icloud.com>
2019-06-13 12:36:09 +0530

    maven cek title

--- com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/seleniumtest.java
+++ com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/seleniumtest.java
@@ -4,8 +4,18 @@ import org.testng.annotations.Test;
 
 public class seleniumtest {
 
-	@Test
+	@Test(priority = 1)
 	public void test() {
 		System.out.println("Test");
+	}
+	
+	@Test(priority = 2)
+	public void tests() {
+		System.out.println("Test2");
+	}
+	
+	@Test
+	public void testss() {
+		System.out.println("Test5");
 	}
 }
