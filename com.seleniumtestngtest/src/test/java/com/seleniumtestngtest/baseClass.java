fed8660a85853e0284c432fbc077653ac9779d81
Fransiskusandika <dikakoko@icloud.com>
2019-06-13 13:07:46 +0530

    oop

--- com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/baseClass.java
+++ com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/baseClass.java
@@ -4,27 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class baseClass {
 	WebDriver driver;
 
-	@Test(priority = 1)
+	@BeforeMethod
 	public void setUp() {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.get("http://localhost/program-toko/");
-		String title = driver.getTitle();
-		System.out.println("Page Title : " + title);
-		Assert.assertEquals(title, "Toko Aksessori Komputer dan Gadget");
-		
 	}
 	
-	@Test(priority = 2)
+	@AfterMethod
 	public void setDown() {
 		driver.quit();
 	}
---
+++ com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/mainClass.java
@@ -0,0 +1,16 @@
+package com.seleniumtestngtest;
+
+import org.testng.annotations.*;
+
+@Test
+public class mainClass extends baseClass {
+
+	@Test(priority = 0)
+	public void Title() throws InterruptedException {
+		cekTitle page = new cekTitle(driver);
+		driver.get("http://localhost/program-toko/");
+		page.title();
+	}
+
+	
+}
