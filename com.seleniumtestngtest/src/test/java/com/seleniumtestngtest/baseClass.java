674061c95991ebae9fb61d16d4f2424d9d294bc9
Fransiskusandika <dikakoko@icloud.com>
2019-06-13 12:59:07 +0530

    cek title

--- com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/baseClass.java
+++ com.seleniumtestngtest/src/test/java/com/seleniumtestngtest/baseClass.java
@@ -4,21 +4,27 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class baseClass {
 	WebDriver driver;
 
-	@BeforeMethod
+	@Test(priority = 1)
 	public void setUp() {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
+		driver.get("http://localhost/program-toko/");
+		String title = driver.getTitle();
+		System.out.println("Page Title : " + title);
+		Assert.assertEquals(title, "Toko Aksessori Komputer dan Gadget");
+		
 	}
 	
-	@AfterMethod
+	@Test(priority = 2)
 	public void setDown() {
 		driver.quit();
 	}
